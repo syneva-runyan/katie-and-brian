c8041dc48dba9b2843510dbc8a47ffbc01faa2d6
syneva-runyan <srunyan3@gatech.edu>
2020-02-13 23:43:25 -0500

    removed history fallback

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -184,7 +184,6 @@ module.exports = function(webpackEnv) {
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
       publicPath: publicPath,
-      historyApiFallback: true,
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
