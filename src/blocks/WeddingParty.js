27a13e9c6c67fdff32f7eab1132b6ab24fd2c6a4
syneva-runyan <srunyan3@gatech.edu>
2020-03-08 13:28:24 -0400

    Finished pics and names

--- src/blocks/WeddingParty.js
+++ src/blocks/WeddingParty.js
@@ -30,10 +30,11 @@ export default function WeddingParty() {
             <h1>Wedding Party</h1>
             <div className="wedding-party">
                 <WeddingPartyPerson name="Katie and Brian" relation="Bride & Groom" imgSrc="../images/weddingParty/katieAndBrian.jpg" />
-                <WeddingPartyPerson name="Syneva Runyan" relation="Katie's Sister" imgSrc="../images/weddingParty/katieAndSyneva.jpg" isDark />
                 <WeddingPartyPerson name="Joey Remsen" relation="Jersey kids" imgSrc="../images/weddingParty/brianAndJoey.jpg" isDark />
-                <WeddingPartyPerson name="Annie Kim" relation="Georgia Tech" imgSrc="../images/weddingParty/katieAndAnnie.jpg" isDark />
+                <WeddingPartyPerson name="Syneva Runyan" relation="Katie's Sister" imgSrc="../images/weddingParty/katieAndSyneva.jpg" isDark />
                 <WeddingPartyPerson name="Benjamin Runyan" relation="New Brother in Law" imgSrc="../images/weddingParty/benji.jpg" />
+                <WeddingPartyPerson name="Annie Kim" relation="Georgia Tech" imgSrc="../images/weddingParty/katieAndAnnie.jpg" isDark />
+                <WeddingPartyPerson name="Jeff Petillo" relation="High School Friend" imgSrc="../images/weddingParty/jeffAndBrian.jpg" isDark/>
                 <WeddingPartyPerson name="Gabrielle Wickizer" relation="New Sister in Law" imgSrc="../images/weddingParty/gabbyAndKatie.jpg" isDark />
                 <WeddingPartyPerson name="Luke Runyan" relation="New Borther in Law" imgSrc="../images/weddingParty/luke.jpg" isDark />
                 <WeddingPartyPerson name="Courtney Olson" relation="CapOne life" imgSrc="../images/weddingParty/courtneyAndKatie.jpg" />
@@ -41,8 +42,8 @@ export default function WeddingParty() {
                 <WeddingPartyPerson name="Esther Kim" relation="DC Roomate" imgSrc="../images/weddingParty/katieAndEsther.jpg" isDark />
                 <WeddingPartyPerson name="Pramod Thammaiah" relation="Jersey Kid" imgSrc="../images/weddingParty/brianAndPromode.jpg" />
                 <WeddingPartyPerson name="Heta Jangla" relation="Helluva engineer" imgSrc="../images/weddingParty/katieAndHeta.jpg" isDark />
-                <WeddingPartyPerson name="Rémi Cossart" relation="Jersey Kid" imgSrc="../images/weddingParty/RemiAndBrian.jpg" />
-                <WeddingPartyPerson name="Gen Wickizer" relation="New Sister in Law" imgSrc="../images/weddingParty/gen.jpg" isDark />
+                <WeddingPartyPerson name="Tyler Stearns" relation="College Friend/Roommate" imgSrc="../images/weddingParty/tylerAndBrian.jpg" isDark/>
+                <WeddingPartyPerson name="Gen Wickizer" relation="New Sister in Law" imgSrc="../images/weddingParty/genAndKatie.png" isDark />
             </div>
         </div>
     );
