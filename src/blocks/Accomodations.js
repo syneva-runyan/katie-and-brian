62bd73660f82c52ae6c154110a302e1c857b3f58
syneva-runyan <srunyan3@gatech.edu>
2020-03-08 19:54:14 -0400

    updated accomodation information

--- src/blocks/Accomodations.js
+++ src/blocks/Accomodations.js
@@ -5,11 +5,16 @@ import './Accomodations.css';
 export default function Accomodations() {
     return (
         <div className="contentContainer">
-            <h1>
+            <h1 className="accomodations__title">
                 Accomodations
             </h1>
-            <p>There is a wedding block reserved at the <a href="https://secure3.hilton.com/en_US/hi/reservation/book.htm?inputModule=HOTEL&ctyhocn=ATLPGHH&spec_plan=RWB&arrival=20200904&departure=20200906&cid=OM,WW,HILTONLINK,EN,DirectLink&fromId=HILTONLINKDIRECT" target="_blank" rel="noreferrer noopener">Peachtree City Hotel and Conference Center</a></p>
-            <p>In addition to the wedding block, there are numerous other hotels and a variety of Air Bnbs in the area.</p>
+            <h2>Hotel Block</h2>
+            <p>We have a block of rooms reserved at the <a href="https://secure3.hilton.com/en_US/hi/reservation/book.htm?inputModule=HOTEL&ctyhocn=ATLPGHH&spec_plan=RWB&arrival=20200904&departure=20200906&cid=OM,WW,HILTONLINK,EN,DirectLink&fromId=HILTONLINKDIRECT" target="_blank" rel="noreferrer noopener">Peachtree City Hotel and Conference Center</a><br/><br/>
+            Rates are $119 per night for either 1 King bed or 2 Queen beds. The hotel is a 5 min drive from the church and a 10 min drive from the reception venue.<br/><br/>
+            Transportation will be provided to and from the hotel and the reception.
+            </p>
+            <h2>Alternate Accommodations</h2>
+            <p>In addition to the wedding block, there are numerous other hotels and a variety of <a href="https://www.airbnb.com/s/Walt-Banks-Road--Peachtree-City--GA--USA/homes?refinement_paths%5B%5D=%2Fhomes&source=mc_search_bar&search_type=autocomplete_click&screen_size=large&checkin=2020-09-04&checkout=2020-09-06&adults=1&query=Walt%20Banks%20Road%2C%20Peachtree%20City%2C%20GA%2C%20USA&place_id=EihXYWx0IEJhbmtzIFJvYWQsIFBlYWNodHJlZSBDaXR5LCBHQSwgVVNBIi4qLAoUChIJU58O6YLp9IgRarXTdTwtdLMSFAoSCZtWa96RwfSIEapGfz1grP2U" target="_blank" rel="noreferrer noopener">Airbnbs</a> in the area.</p>
             <p>Looking for <NavLink className="accomodations__things-to-do" to="/things-to-do">things to do while in Peachtree City?</NavLink></p>
         </div>
     );
