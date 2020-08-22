2f772789d7fef2e64a813a47d2ed9cc654008854
syneva-runyan <srunyan3@gatech.edu>
2020-08-22 16:11:34 -0400

    Add live stream banner and link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  NavLink,
-  Switch,
-  Route,
+    BrowserRouter as Router,
+    NavLink,
+    Switch,
+    Route,
 } from "react-router-dom";
-import { Accomodations, RSVP, Intro, EventInfo, OurStory, WeddingParty } from './blocks/index.js';
+import { Accomodations, RSVP, Intro, EventInfo, OurStory, WeddingParty, LiveStream } from './blocks/index.js';
 import Nav from './Components/Nav';
 import './App.css';
 import MasonryGrid from './Components/MasonryGrid';
@@ -17,67 +17,112 @@ import SeeRSVPs from './blocks/SeeRSVPs';
 
 // example photo gallery images
 let images = [];
-for(let i = 0; i<= 40; i++){
-	images.push(`./images/photo-gallery/${i}.jpg`);
+for (let i = 0; i <= 40; i++) {
+    images.push(`./images/photo-gallery/${i}.jpg`);
 }
 
 shuffle(images);
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="covidBanner">
-            <NavLink to="/covid-19">COVID-19 Updates</NavLink>
-        </div>
-        <Switch>
-          <Route path="/rsvp">
-            <Nav/>
-            <RSVP />
-          </Route>
-          <Route path="/event-info">
-            <Nav />
-            <EventInfo />
-          </Route>
-          <Route path="/accomodations">
-            <Nav />
-            <Accomodations />
-          </Route>
-          <Route path="/wedding-party">
-            <Nav />
-            <WeddingParty />
-          </Route>
-          <Route path="/our-story">
-            <Nav />
-            <OurStory />
-          </Route>
-          <Route path="/photos">
-            <Nav />
-            <MasonryGrid breakPoints={[350, 500, 750]} images={images} />
-          </Route>
-          <Route path="/registry">
-            <Nav />
-            <ComingSoon />
-          </Route>
-          <Route path="/things-to-do">
-            <Nav />
-            <ComingSoon />
-          </Route>
-          <Route path="/covid-19">
-            <Nav />
-            <Covid19 />
-          </Route>
-          <Route path="/see-rsvps">
-            <SeeRSVPs />
-          </Route>
-          <Route exact path="/">
-            <Intro />
-            <Nav alwaysExpanded />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    return ( <
+        Router >
+        <
+        div className = "App" >
+        <
+        div className = "covidBanner" >
+        <
+        NavLink to = "/live-stream" > Live Stream < /NavLink> <
+        /div> <
+        Switch >
+        <
+        Route path = "/live-stream" >
+        <
+        Nav / >
+        <
+        LiveStream / >
+        <
+        /Route> <
+        Route path = "/rsvp" >
+        <
+        Nav / >
+        <
+        RSVP / >
+        <
+        /Route> <
+        Route path = "/event-info" >
+        <
+        Nav / >
+        <
+        EventInfo / >
+        <
+        /Route> <
+        Route path = "/accomodations" >
+        <
+        Nav / >
+        <
+        Accomodations / >
+        <
+        /Route> <
+        Route path = "/wedding-party" >
+        <
+        Nav / >
+        <
+        WeddingParty / >
+        <
+        /Route> <
+        Route path = "/our-story" >
+        <
+        Nav / >
+        <
+        OurStory / >
+        <
+        /Route> <
+        Route path = "/photos" >
+        <
+        Nav / >
+        <
+        MasonryGrid breakPoints = {
+            [350, 500, 750] }
+        images = { images }
+        /> <
+        /Route> <
+        Route path = "/registry" >
+        <
+        Nav / >
+        <
+        ComingSoon / >
+        <
+        /Route> <
+        Route path = "/things-to-do" >
+        <
+        Nav / >
+        <
+        ComingSoon / >
+        <
+        /Route> <
+        Route path = "/covid-19" >
+        <
+        Nav / >
+        <
+        Covid19 / >
+        <
+        /Route> <
+        Route path = "/see-rsvps" >
+        <
+        SeeRSVPs / >
+        <
+        /Route> <
+        Route exact path = "/" >
+        <
+        Intro / >
+        <
+        Nav alwaysExpanded / >
+        <
+        /Route> <
+        /Switch> <
+        /div> <
+        /Router>
+    );
 }
 
 export default App;
---
+++ src/blocks/LiveStream.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './LiveStream.css';
+
+const LiveStream = function() {
+    return ( <
+        div className = "contentContainer live_stream" >
+        <
+        h1 > Live Stream < /h1> <
+        div className = "live_stream_image" >
+        <
+        img src = '../images/steps.jpg'
+        alt = "Katie and Brian" / >
+        <
+        /div> <
+        div className = "live_stream_content" >
+        <
+        div className = "live_stream_main_block" >
+        <
+        strong >
+        <
+        span className = "main-text" > The wedding will be livestreamed on <
+        a target = "_blank"
+        rel = "noopener noreferrer"
+        href = "https://www.youtube.com/channel/UC1ee7lmibIVuUQhMuqyCgrQ" >
+        Holy Trinity's youtube channel.</a></span></strong></div> <
+        p >
+        If watching virtually, you are encouraged to hashtag a picture of yourself watching with < strong > #betterWickYou < /strong> <
+        br / > < br / >
+        Information about various zoom watch parties will be posted on this page closer to the wedding as well. < /p>< /
+        div > < /
+        div >
+    );
+};
+
+export default LiveStream;
